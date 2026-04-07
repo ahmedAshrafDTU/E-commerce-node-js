53ed41cb39fd2c9094cf40a31861c07c246649f6
ahmedAshrafDTU <118565813+ahmedAshrafDTU@users.noreply.github.com>
2026-04-07 15:23:04 +0200

    feat: implement centralized error handling, async middleware, and category controller logic

--- src/utils/middleware/catchAsyncError.js
+++ src/utils/middleware/catchAsyncError.js
@@ -1,9 +1,9 @@
-const catchAsyncError = (fn)=>{
-  return (req,res,next)=>{
-    fn(req,res,next).catch(err=>{
-      next(err)
-    })
-  }
-}
+const catchAsyncError = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch((err) => {
+      next(err);
+    });
+  };
+};
 
-export default catchAsyncError;
+module.exports = catchAsyncError;
