67dfa80ada0bd18fd857e030d5ab1ad92816df07
ahmedAshrafDTU <118565813+ahmedAshrafDTU@users.noreply.github.com>
2026-04-07 13:59:44 +0200

    fix: update getCategoryById to handle category not found and response messages

--- src/modules/categories/categories.controller.js
+++ src/modules/categories/categories.controller.js
@@ -26,10 +26,13 @@ const getAllCategories = async (req, res) => {
 const getCategoryById = async (req, res) => {
   try {
     const category = await Category.findById(req.params.id);
-    if (!category) {
-      return res.status(404).json({ message: "Category not found" });
-    }
-    res.status(200).json({ message: "Success", category });
+    /* if (!category) {
+       return res.status(404).json({ message: "Category not found" });
+     }
+     res.status(200).json({ message: "Success", category });
+     */
+    !category && res.status(404).json({ message: "Category not found" });
+    category && res.status(200).json({ message: "Category deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error fetching category", error: error.message });
   }
